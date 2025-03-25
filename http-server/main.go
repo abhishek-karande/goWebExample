5a8514c13db5c4f45f56b1e394072c7dd3d495cd
Abhishek Karande <akarande70@gmail'.com>
2025-03-26 03:06:29 +0530

    added http server code with static files

---
+++ http-server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website %s", r.URL.Path)
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":6969", nil)
+}
